26e1c7bf77777603c0a3e13541bfaa36d4d54d34
rthangaraj <ramesh.thangaraj@gmail.com>
2021-04-01 17:07:56 +0100

    docker and monitoring and infra provisioning

---
+++ azure-dep-tf/modules/securitygroup/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_network_security_group" "web-nsg" {
+  name                = "web-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group
+  
+  security_rule {
+    name                       = "ssh-rule-1"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "22"
+  }
+  
+  security_rule {
+    name                       = "ssh-rule-2"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_address_prefix      = "192.168.3.0/24"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "22"
+}
+}
+
+resource "azurerm_subnet_network_security_group_association" "web-nsg-subnet" {
+  subnet_id                 = var.web_subnet_id
+  network_security_group_id = azurerm_network_security_group.web-nsg.id
+}
+
+
+
---
+++ azure-dep-tf/terraform.tfvars
@@ -0,0 +1,8 @@
+name = "three-tier-azurestack"
+location = "North Europe"
+vnetcidr = "192.168.0.0/16"
+websubnetcidr = "192.168.1.0/24"
+web_host_name = "webserver"
+web_username = "web_user"
+web_os_password = "@Webuser1@2020!"
+
