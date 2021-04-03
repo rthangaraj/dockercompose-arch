ff4d7cdcddc248e61ea022ff668be28b2b090983
rthangaraj <ramesh.thangaraj@gmail.com>
2021-04-03 23:19:37 +0100

    added healthcheck script

---
+++ monitoring/healthcheck.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/sh
+
+function check {
+     if [ $? -ne 0 ] ; then
+         echo "Error occurred getting URL $1:"
+         if [ $? -eq 6 ]; then
+             echo "Unable to resolve host"
+         fi
+         if [$? -eq 7 ]; then
+             echo "Unable to connect to host"
+         fi
+         exit 1
+     fi
+
+}
+curl -s -o "/dev/null" $1
+check;
